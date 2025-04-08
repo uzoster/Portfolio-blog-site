64e175fd6b97249e0d15e197d1a129aaf9b1a91f
uzoster <asadullaxamidullayev4@gmail.com>
2025-04-08 22:28:49 +0500

    Nextjs-contentlayer-blog

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'AstroBayt',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'asadullaxamidullayev4@gmail.com', 
+    github: 'https://github.com/uzoster',
+    twitter: 'https://t.me/AstroBayt',
+    facebook: 'https://t.me/AstroBayt',
+    youtube: 'https://t.me/AstroBayt',
+    linkedin: 'https://t.me/AstroBayt',
+    dribbble: 'https://t.me/AstroBayt',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
